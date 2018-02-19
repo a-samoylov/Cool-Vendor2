c9ec67401fbb2e51474ca83051c86fb2d75389cb
imperatorUA <a.samoylovua@gmail.com>
2018-02-19 03:06:22 +0200

    create controller

---
+++ app/code/ASI/SomeAPI/Controller/Index/Index.php
@@ -0,0 +1,22 @@
+<?php
+namespace ASI\SomeAPI\Controller\Index;
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+	protected $_pageFactory;
+
+	public function __construct(
+		\Magento\Framework\App\Action\Context $context,
+		\Magento\Framework\View\Result\PageFactory $pageFactory)
+	{
+		$this->_pageFactory = $pageFactory;
+		return parent::__construct($context);
+	}
+
+	public function execute()
+	{
+        $post = $this->getRequest()->getPostValue();
+		echo "Hello World1";
+		//exit;
+	}
+}
---
+++ app/code/ASI/SomeAPI/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+	\Magento\Framework\Component\ComponentRegistrar::MODULE,
+	'ASI_SomeAPI',
+	__DIR__
+);
